68fcab0d03819c7f9f0cc7928215e5389685c739
Eslam Khaled <eslamkhaled64@gmail.com>
2021-03-28 03:06:07 +0200

    Fix: canny args

--- Task 1/Filters.py
+++ Task 1/Filters.py
@@ -72,22 +72,23 @@ class Filter:
         pass
 
     @staticmethod
-    def canny(img: np.ndarray, direction="x") -> np.ndarray:
-        """canny: The Process of Canny edge detection algorithm can be broken down to 5 different steps:
+    def canny(img: np.ndarray, min_value: int, max_value: int) -> np.ndarray:
+        """canny [The Process of Canny edge detection algorithm can be broken down to 5 different steps:
 
                     Apply Gaussian filter to smooth the image in order to remove the noise
                     Find the intensity gradients of the image
                     Apply gradient magnitude thresholding or lower bound cut-off suppression to get rid of spurious response to edge detection
                     Apply double threshold to determine potential edges
-                    Track edge by hysteresis: Finalize the detection of edges by suppressing all the other edges that are weak and not connected to strong edges.
+                    Track edge by hysteresis: Finalize the detection of edges by suppressing all the other edges that are weak and not connected to strong edges.]
 
         Args:
             img (np.ndarray): [description]
-            direction (str, optional): [description]. Defaults to "x".
+            min_value (int): [description]
+            max_value (int): [description]
 
         Returns:
             np.ndarray: [description]
-        """
+        """        
         pass
 
     # https://www.youtube.com/watch?v=9mLeVn8xzMw
