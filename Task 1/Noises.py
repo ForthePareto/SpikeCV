70a6a466ffbc9137080e32aa6166d16ac5656b27
Eslam Khaled <eslamkhaled64@gmail.com>
2021-03-29 07:45:45 +0200

    Add gaussian noise

--- Task 1/Noises.py
+++ Task 1/Noises.py
@@ -1,14 +1,24 @@
 import numpy as np
-
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
 class Noise:
-    def __init__(self, img):
-        return 0
+    
     @staticmethod
     def uniform(image: np.ndarray) -> np.ndarray:
         pass
     @staticmethod    
-    def gaussian(image: np.ndarray) -> np.ndarray:
-        pass
+    def gaussian(image: np.ndarray,mean=0,variance=0.1) -> np.ndarray:
+        sigma = variance**0.5
+        gauss = np.random.normal(mean,sigma,(image.shape))
+        gauss = gauss.reshape(image.shape)*np.max(img)
+        noisy = image + gauss
+        return noisy
     @staticmethod
     def salt_pepper(image: np.ndarray) -> np.ndarray:
         pass
+
+if __name__ == '__main__':
+    img = mpimg.imread("EvV4-uOWYAQOM1x.jpg",)
+    noisy = Noise.gaussian(img)
+    plt.imshow(noisy,cmap="gray")
+    plt.show()
