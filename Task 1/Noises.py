a044fe88b65a16ea0e95489a15faaaafbc663958
Eslam Khaled <eslamkhaled64@gmail.com>
2021-03-29 23:49:51 +0200

    Add uniform and salt_pepper noise

--- Task 1/Noises.py
+++ Task 1/Noises.py
@@ -1,24 +1,45 @@
 import numpy as np
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
+
+
 class Noise:
-    
+
     @staticmethod
-    def uniform(image: np.ndarray) -> np.ndarray:
-        pass
-    @staticmethod    
-    def gaussian(image: np.ndarray,mean=0,variance=0.1) -> np.ndarray:
+    def uniform(image: np.ndarray, amount=0.2) -> np.ndarray:
+        noise = amount*np.random.uniform(0, 255, size=image.shape)
+        output = (noise + image).astype(np.uint8)
+        return output
+
+    @staticmethod
+    def gaussian(image: np.ndarray, mean=0, variance=0.1) -> np.ndarray:
         sigma = variance**0.5
-        gauss = np.random.normal(mean,sigma,(image.shape))
+        gauss = np.random.normal(mean, sigma, (image.shape))
         gauss = gauss.reshape(image.shape)*np.max(img)
         noisy = image + gauss
         return noisy
+
     @staticmethod
-    def salt_pepper(image: np.ndarray) -> np.ndarray:
-        pass
+    def salt_pepper(image: np.ndarray, salt_ratio=0.5, amount=0.01) -> np.ndarray:
+        output = np.copy(image)
+        # Salt mode
+        num_salt = np.ceil(amount * image.size * salt_ratio)
+        coords = [np.random.randint(0, i - 1, int(num_salt))
+                  for i in image.shape]
+        output[tuple(coords)] = 255
+
+        # Pepper mode
+        num_pepper = np.ceil(amount * image.size * (1. - salt_ratio))
+        coords = [np.random.randint(0, i - 1, int(num_pepper))
+                  for i in image.shape]
+        output[tuple(coords)] = 0
+        return output
+
 
 if __name__ == '__main__':
-    img = mpimg.imread("EvV4-uOWYAQOM1x.jpg",)
-    noisy = Noise.gaussian(img)
-    plt.imshow(noisy,cmap="gray")
+    img = mpimg.imread("emHn_NO-.jpg",)
+    noisy = Noise.uniform(img)
+    # noisy = Noise.gaussian(img)
+    # noisy = Noise.salt_pepper(img)
+    plt.imshow(noisy, cmap="gray")
     plt.show()
