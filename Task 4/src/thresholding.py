642c8e6f2771d2e98495cdf178a2e7ce119d9d9b
Eslam Khaled <eslamkhaled64@gmail.com>
2021-05-22 08:19:56 +0200

    Add docs, API

--- Task 4/src/thresholding.py
+++ Task 4/src/thresholding.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 from Filters import gray
+np.seterr(divide='ignore', invalid='ignore')
 
 
 class Thresholding:
@@ -9,18 +10,36 @@ class Thresholding:
     Thresholding API
     """
     @staticmethod
-    def otsu(img: np.ndarray, scope="global") -> np.ndarray:
+    def bimodal(img: np.ndarray, scope="global", block_length=25) -> np.ndarray:
         """
         local and global bimodal thresholding 
         """
-        pass
+        scope = scope.lower()
+        if scope not in ["global", "local"]:
+            raise ValueError(
+                "Undefined scope, please choose either global or local")
+        img = gray(img)
+        OtsuThresholder = Otsu(img)
+        if scope == "global":
+            return OtsuThresholder.global_thresholding()
+        elif scope == "local":
+            return OtsuThresholder.local_thresholding(block_length=block_length)
 
     @staticmethod
-    def spectral(img: np.ndarray, scope="global") -> np.ndarray:
+    def spectral(img: np.ndarray, scope="global", block_length=25) -> np.ndarray:
         """
         local and global multimodal thresholding 
         """
-        pass
+        scope = scope.lower()
+        if scope not in ["global", "local"]:
+            raise ValueError(
+                "Undefined scope, please choose either global or local")
+        img = gray(img)
+        OtsuThresholder = Otsu(img)
+        if scope == "global":
+            return OtsuThresholder.global_spectral_thresholding()
+        elif scope == "local":
+            return OtsuThresholder.local_spectral_thresholding(block_length=block_length)
 
 
 class Otsu:
@@ -30,98 +49,85 @@ class Otsu:
         self.thresholds = []
 
     def global_thresholding(self, img=None, n_bins=255, plot=False):
-        """Finding thresholds for bi/multispectral thresholding using Otsu
+        """Finding thresholds and applying bimodal thresholding using Otsu
         """
         if img is None:
             img = self.img
         # get pixels intensities distribution
         n_bins = int(np.max(img))
-        print(n_bins)
         histogram1, bin_edges = np.histogram(img, bins=n_bins)
 
-        # print(histogram1.shape, bin_edges.shape)
-        # Normalize distribution to get the probabilities of each pixel intensity
-        histogram = np.divide(histogram1.reshape(-1), histogram1.max())
-        bin_mids = (bin_edges[:-1] + bin_edges[1:]) / 2.
-        # print(bin_mids)
-        # Iterate over all thresholds (indices) and get the probabilities w1(t), w2(t)
-        weight1 = np.cumsum(histogram)
-        weight2 = np.cumsum(histogram[::-1])[::-1]
-
-        # Get the class means mu0(t)
-        mean1 = np.cumsum(histogram * bin_mids) / weight1
-        # Get the class means mu1(t)
-        mean2 = (np.cumsum((histogram * bin_mids)[::-1]) / weight2[::-1])[::-1]
-
-        inter_class_variance = weight1[:-1] * \
-            weight2[1:] * (mean1[:-1] - mean2[1:]) ** 2
-
-        sorted_threshs = np.argsort(inter_class_variance)[::-1]
-        # print(sorted_threshs)
-
-        # Maximize the inter_class_variance function val
-        index_of_max_val = np.argmax(inter_class_variance)
-        threshold = bin_mids[:-1][index_of_max_val]
+        threshold, _, _ = self.get_best_threshold(histogram1, bin_edges)
         # add threshold to the class attributes list of thresholds
         self.thresholds = []
         self.thresholds.append(threshold)
-        # print("thresholding result: ", threshold)
-        result = Otsu._binarize(img, threshold)
+        # binarize(black & white) the image based on the calculated threshold
+        bimodal_segmented_image = Otsu._binarize(img, threshold)
         if plot:
             Otsu._plot_image_historgram_pair(
-                img, result, histogram1, n_bins, self.thresholds)
-        return result
+                img, bimodal_segmented_image, histogram1, n_bins, self.thresholds)
+        return bimodal_segmented_image
 
-    def global_spectral_thresholding(self, img=None,n_bins=255, plot=False):
-        """Finding thresholds for bi/multispectral thresholding using Otsu
+    def global_spectral_thresholding(self, img=None, n_bins=255, plot=False):
+        """Finding thresholds for spectral thresholding using Otsu 
         """
         if img is None:
-            img = self.img
+            img = np.copy(self.img)
         # get pixels intensities distribution
         n_bins = int(np.max(img))
-        print(n_bins)
         histogram1, bin_edges = np.histogram(img, bins=n_bins)
-        threshold,thresholdIdx,_ = self.get_best_threshold(histogram1, bin_edges)
-        threshold_left, _,cost2 = self.get_best_threshold(histogram1[:thresholdIdx], bin_edges[:thresholdIdx+1]) 
-        threshold_right, _,cost3 = self.get_best_threshold(histogram1[thresholdIdx:], bin_edges[thresholdIdx:]) 
-        # add threshold to the class attributes list of thresholds
-        print(cost3.shape)
-        threshold2 = threshold_left if cost2>cost3 else threshold_right
+        # get best threshold that maximizes the between class variances in the given histogram
+        threshold, thresholdIdx, _ = self.get_best_threshold(
+            histogram1, bin_edges)
+        # Find the second threshold so the histogram is splited into 3 regions
+        # choose the best threshold either right or left from the first threshold
+        threshold_left, _, cost2 = self.get_best_threshold(
+            histogram1[:thresholdIdx], bin_edges[:thresholdIdx+1])
+        threshold_right, _, cost3 = self.get_best_threshold(
+            histogram1[thresholdIdx:], bin_edges[thresholdIdx:])
+        # choosing one of the left or right thresholds, based on cost
+        threshold2 = threshold_left if cost2 > cost3 else threshold_right
+
+        # save threshold to the class attributes list of thresholds to be used in histogram pl
         self.thresholds = []
         self.thresholds.append(threshold)
         self.thresholds.append(threshold2)
-        # print("thresholding result: ", threshold)
-        result = Otsu._digitize(img, [threshold,threshold2])
+        # each pixel will get one of 3 colors/intesies based on it's region in the distribution
+        result = Otsu._digitize(img, [threshold, threshold2])
+
         if plot:
             Otsu._plot_image_historgram_pair(
                 img, result, histogram1, n_bins, self.thresholds)
         return result
 
     def get_best_threshold(self, histogram, bin_edges):
-        # Normalize distribution to get the probabilities of each pixel intensity
+        # normalize distribution to get the probabilities of each pixel intensity
         histogram = np.divide(histogram.reshape(-1), histogram.max())
+        # get midpoints of each bin
         bin_mids = (bin_edges[:-1] + bin_edges[1:]) / 2.
-        # print(bin_mids)
         # Iterate over all thresholds (indices) and get the probabilities w1(t), w2(t)
         weight1 = np.cumsum(histogram)
         weight2 = np.cumsum(histogram[::-1])[::-1]
 
         # Get the class means mu0(t)
-        mean1 = np.cumsum(histogram * bin_mids) / weight1
+        mean1 = np.divide(np.cumsum(histogram * bin_mids) , weight1)
         # Get the class means mu1(t)
-        mean2 = (np.cumsum((histogram * bin_mids)[::-1]) / weight2[::-1])[::-1]
+        mean2 = np.divide(np.cumsum((histogram * bin_mids)
+                                    [::-1]), weight2[::-1])[::-1]
 
-        inter_class_variance = weight1[:-1] * \
+        between_class_variance = weight1[:-1] * \
             weight2[1:] * (mean1[:-1] - mean2[1:]) ** 2
 
         # Maximize the inter_class_variance function val
-        index_of_max_val = np.argmax(inter_class_variance)
+        index_of_max_val = np.argmax(between_class_variance)
 
         threshold = bin_mids[:-1][index_of_max_val]
-        return threshold, index_of_max_val , np.max(inter_class_variance)
+        return threshold, index_of_max_val, np.max(between_class_variance)
 
-    def local_thresholding(self, block_length=25, plot=False):
-        img = np.copy(self.img)
+    def local_thresholding(self, img=None, block_length=25, plot=False):
+        if img is None:
+            img = np.copy(self.img)
+
         locally_thresholded = self.local_processing(
             img, radius=block_length, func=self.global_thresholding)
         if plot:
@@ -133,6 +139,7 @@ class Otsu:
             plt.axis("off")
             plt.show()
         return locally_thresholded
+
     def local_spectral_thresholding(self, block_length=25, plot=False):
         img = np.copy(self.img)
         locally_thresholded = self.local_processing(
@@ -147,44 +154,13 @@ class Otsu:
             plt.show()
         return locally_thresholded
 
-    @classmethod
-    def _binarize(cls, img, threshold):
-        result = np.copy(img)
-        result[img > threshold] = 255
-        result[img <= threshold] = 0
-        return result
-    @classmethod
-    def _digitize(cls, img, thresholds):
-        # result = np.dstack((np.copy(img),np.zeros_like(img),np.zeros_like(img)))
-        result = np.copy(img)
-        print(result.shape)
-        low_thresh = np.min(thresholds)
-        high_thresh = np.max(thresholds)
-        result[img <= low_thresh] = 0
-        result[img > high_thresh] = 255
-       
-        result[np.logical_and((low_thresh<=img ),  (img<= high_thresh))] = 128
-        return result
-
-    @classmethod
-    def _plot_image_historgram_pair(cls, img, result, histogram, n_bins, thresholds=[]):
-        f, ax = plt.subplots(nrows=1, ncols=2, figsize=(10, 6))
-        ax[0].imshow(result, cmap="gray")
-        ax[1].set_title("historgram")
-        ax[0].axis("off")
-        ax[1].hist(img.ravel(), bins=range(1, n_bins),
-                   color='#0504ab', alpha=0.55, rwidth=0.57)
-        ax[1].set_title("historgram")
-        print(np.max(histogram))
-        if len(thresholds) > 0:
-            ax[1].vlines(thresholds, 0, np.max(histogram),  color='r')
-        plt.show()
-
     def local_processing(self, img, radius=35, func=None):
         rows, cols = img.shape[0], img.shape[1]
+        # The callback function to be applied
         def f(x): return func(x)
 
         result = np.copy(img)
+        # iterate over image in strided blocks with the given size: (radius,radius)
         for r in range(0, rows-radius, radius):
             for c in range(0, cols-radius, radius):
                 # block = img[r:(min(r + radius, rows)),
@@ -197,13 +173,18 @@ class Otsu:
                 #                                       radius, cols)] = block_proccessed
                 result[r:(r + radius),
                        c:(c + radius)] = block_proccessed
+
+        # Because many images are not in square size, there will be issues in the lower right boundaries
+        # so we apply the function the these boundaries specifically
+
+        # So, apply on the last row of blocks
         for r in range(0, rows-radius, radius):
             block = img[r:(r + radius),
                         cols-radius:]
             block_proccessed = f(block)
             result[r:(r + radius),
                    cols-radius:] = block_proccessed
-
+        # apply on the last column of blocks
         for c in range(0, cols-radius, radius):
             block = img[rows-radius:,
                         c:(c + radius)]
@@ -211,15 +192,55 @@ class Otsu:
             block_proccessed = f(block)
             result[rows-radius:,
                    c:(c + radius)] = block_proccessed
+        block = img[rows-radius:,
+                    cols-radius:]
+        # apply on the lowest right corner block
+        block_proccessed = f(block)
+        result[rows-radius:,
+               cols-radius:] = block_proccessed
 
         return result
 
+    @classmethod
+    def _binarize(cls, img, threshold):
+        result = np.copy(img)
+        result[img >= threshold] = 255
+        result[img < threshold] = 0
+        return result
+
+    @classmethod
+    def _digitize(cls, img, thresholds):
+        # result = np.dstack((np.copy(img),np.zeros_like(img),np.zeros_like(img)))
+        result = np.copy(img)
+        low_thresh = np.min(thresholds)
+        high_thresh = np.max(thresholds)
+        result[img <= low_thresh] = 0
+        result[img > high_thresh] = 255
+
+        result[np.logical_and((low_thresh <= img),
+                              (img <= high_thresh))] = 128
+        return result
+
+    @classmethod
+    def _plot_image_historgram_pair(cls, img, result, histogram, n_bins, thresholds=[]):
+        f, ax = plt.subplots(nrows=1, ncols=2, figsize=(10, 6))
+        ax[0].imshow(result, cmap="gray")
+        ax[1].set_title("historgram")
+        ax[0].axis("off")
+        ax[1].hist(img.ravel(), bins=range(1, n_bins),
+                   color='#0504ab', alpha=0.55, rwidth=0.57)
+        ax[1].set_title("historgram")
+        if len(thresholds) > 0:
+            ax[1].vlines(thresholds, 0, np.max(histogram),  color='r')
+        plt.show()
+
 
 if __name__ == '__main__':
-    img = mpimg.imread("testImgs/gray3.jpg")
+    img = mpimg.imread("testImgs/Lenna.jpg")
     img = gray(img)
     OtsuThresholder = Otsu(img)
     # OtsuThresholder.global_thresholding(plot=True)
-    OtsuThresholder.global_spectral_thresholding(plot=True)
-    OtsuThresholder.local_spectral_thresholding(plot=True,block_length=77)
-    # OtsuThresholder.local_thresholding(block_length=75,plot=True)
+    # OtsuThresholder.global_spectral_thresholding(plot=True)
+    # OtsuThresholder.local_spectral_thresholding(plot=True, block_length=77)
+    OtsuThresholder.local_thresholding(block_length=77,plot=True)
+    # Thresholding.spectral(img, scope="global")
